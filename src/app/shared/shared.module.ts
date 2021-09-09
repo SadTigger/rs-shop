b52b299ffa8de1c2a46565b1928c522d468e4be2
Pavel Zhachko <rammiel@yandex.ru>
2021-09-09 18:38:16 +0300

    feat: add material module to contain angular materials; fix: shared module import/export

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,10 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { MaterialModule } from "./material/material.module";
 
 @NgModule({
   declarations: [],
-  imports: [],
+  imports: [CommonModule, MaterialModule],
+  exports: [CommonModule, MaterialModule],
 })
 export class SharedModule {}
