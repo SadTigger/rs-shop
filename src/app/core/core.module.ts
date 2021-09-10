bbc426616b1fb8e9f635fb1acdf8f6a8c474277b
Pavel Zhachko <rammiel@yandex.ru>
2021-09-10 21:39:34 +0300

    feat: add working-time component to header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,11 +3,13 @@ import { SharedModule } from "../shared/shared.module";
 import { CommunicationsComponent } from "./components/header/communications/communications.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { LocationComponent } from "./components/header/location/location.component";
+import { WorkingTimeComponent } from "./components/header/working-time/working-time.component";
 
 const CoreComponents = [
   HeaderComponent,
   LocationComponent,
   CommunicationsComponent,
+  WorkingTimeComponent,
 ];
 
 @NgModule({
